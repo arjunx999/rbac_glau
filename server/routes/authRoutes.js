37c45f604706354c4b7cb47d68c1c3b3f4ec2ce3
arjunx999 <arjun.001573@gmail.com>
2026-04-15 00:11:06 +0530

    added authorization for register user

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import { register, login, getMe } from "../controllers/authController.js";
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
+import authorize from "../middleware/authorize.js";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", authorize("ADMIN"), verifyToken, register);
 
 router.post("/login", login);
 
